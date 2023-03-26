c35c0513086daca51f708e9a498048ec7a913d99
Yuji Nakamura <ny5117@icloud.com>
2023-03-26 23:04:09 +0900

    ドメインの型を定義

---
+++ src/store/categories.ts
@@ -0,0 +1,20 @@
+import { CategoryType } from '../types/types';
+
+export const categories: CategoryType[] = [
+  {
+    id: 1,
+    name: 'Mountain',
+  },
+  {
+    id: 2,
+    name: 'Beach',
+  },
+  {
+    id: 3,
+    name: 'Birds',
+  },
+  {
+    id: 4,
+    name: 'Food',
+  },
+];
